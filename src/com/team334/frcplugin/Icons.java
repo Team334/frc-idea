459cf6d5b4ab2599e3584b0a41f75c82b24778ca
zelkatani <zelkatani@gmail.com>
2017-01-27 15:03:23 -0500

    Initial Commit

---
+++ src/com/team334/frcplugin/Icons.java
@@ -0,0 +1,18 @@
+package com.team334.frcplugin;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public final class Icons {
+
+    private Icons() {}
+
+    public static final Icon BUTTON = IconLoader.findIcon("icons/Button.png");
+    public static final Icon COMMAND = IconLoader.findIcon("icons/Command.png");
+    public static final Icon COMMAND_GROUP = IconLoader.findIcon("icons/CommandGroup.png");
+    public static final Icon GAZEBO = IconLoader.findIcon("icons/Gazebo.png");
+    public static final Icon PID_SUBSYSTEM = IconLoader.findIcon("icons/PIDSubsystem.png");
+    public static final Icon SUBSYSTEM = IconLoader.findIcon("icons/Subsystem.png");
+    public static final Icon WPI_ICON = IconLoader.findIcon("icons/wpi.ico" );
+}
